a622b82239010090125f75aea78181a15747aaef
arodr856 <arodr856@gmail.com>
2019-04-27 16:16:02 -0700

    block motion compensation initial commit

---
+++ homework/homework03/CS4551_Rodriguez.java
@@ -0,0 +1,23 @@
+
+public class CS4551_Rodriguez{
+    public static void main(String[] args){
+        
+    }
+
+    public static void seperateImage(MImage refImg, MImage targetImg){
+
+        int rowSize = targetImg.getH() / 16;
+        int colSize = targetImg.getW() / 16;
+
+
+        for(int row = 0; row < targetImg.getH(); row += 16){
+            for(int col = 0; col < targetImg.getW(); col += 16){
+
+                
+
+            }   
+        }
+
+    }
+
+}
